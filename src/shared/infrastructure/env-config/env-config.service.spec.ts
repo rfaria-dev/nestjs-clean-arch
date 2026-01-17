1a126397c78a69a876d98af35495f68ffea954bf
Roger Faria2 <rfaria2@lenovo.com>
2026-01-17 17:27:01 -0300

    feat: implementing EnvConfig interface

--- src/shared/infrastructure/env-config/env-config.service.spec.ts
+++ src/shared/infrastructure/env-config/env-config.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EnvConfigService } from './env-config.service';
 
 describe('EnvConfigService', () => {
-  let service: EnvConfigService;
+	let service: EnvConfigService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EnvConfigService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [EnvConfigService],
+		}).compile();
 
-    service = module.get<EnvConfigService>(EnvConfigService);
-  });
+		service = module.get<EnvConfigService>(EnvConfigService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
